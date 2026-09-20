ffae1b9595a419757b1132146b6e47b4d7cf92a3
moon044 <Kmr040506@naver.com>
2026-09-20 20:27:31 +0900

    chore: 공통 테마 및 텍스트 스타일 적용

---
+++ moon/week01/lib/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const primary = Color(0xFF6750A4);
+  static const background = Color(0xFFFAF9F5);
+  static const textPrimary = Color(0xFF1C1B1F);
+  static const textSecondary = Color(0xFF79747E);
+  static const white = Color.fromARGB(255, 255, 255, 255);
+  static const lightviolet = Color.fromARGB(255, 232, 219, 255);
+}
