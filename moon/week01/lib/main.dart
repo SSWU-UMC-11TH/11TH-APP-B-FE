cece21989a7acae16d1ea14acaa013c8ca65cf5a
moon044 <168537106+moon044@users.noreply.github.com>
2026-09-27 01:28:21 +0900

    Update main.dart

--- moon/week01/lib/main.dart
+++ moon/week01/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/profile_screen.dart';
+import 'screens/start_screen.dart';
 import 'theme/app_theme.dart';
 
 void main() {
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'MovieLog',
       theme: AppTheme.light,
-      home: const ProfileScreen(),    );
+      home: const StartScreen(),    
+    );
   }
 }
