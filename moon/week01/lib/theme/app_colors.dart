8c1c4aad62f9f3af2572597ac75fbbd25151bf77
moon044 <168537106+moon044@users.noreply.github.com>
2026-09-27 01:03:01 +0900

    Update app_colors.dart

--- moon/week01/lib/theme/app_colors.dart
+++ moon/week01/lib/theme/app_colors.dart
@@ -7,6 +7,7 @@ class AppColors {
   static const background = Color(0xFFFAF9F5);
   static const textPrimary = Color(0xFF1C1B1F);
   static const textSecondary = Color(0xFF79747E);
+  static const bodyText = Color(0xFF525054);
   static const white = Color.fromARGB(255, 255, 255, 255);
   static const lightviolet = Color.fromARGB(255, 232, 219, 255);
 }
